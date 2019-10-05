6fae15560cde232108fff253b5fe1bbefeb8ef68
Maximiliano Fuentes Nuñez <mfuentes@rayensalud.com>
2019-10-05 20:01:58 -0300

    Subiendo video stream

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = new express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+var Log = require('log'),
+log = new Log('debug')
+
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.redirect('index.html');
+});
+
+io.on('connection', function(socket){
+    socket.on('stream', function(img){
+        socket.broadcast.emit('stream', img);
+    });
+})
+
+http.listen(port, function(){
+    console.log('Server puerto %s', port);
+});
